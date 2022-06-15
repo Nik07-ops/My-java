4bf5b904153ce35f1bec3f9484f0f9474836ea04
Nikhil Kumar <nikhilsingh86511@gmail.com>
2022-06-15 11:50:35 +0530

    pattern

---
+++ MidElementArray.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+class MidElementArray{
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter size of array");
+            int n=sc.nextInt();
+            int a[]=new int[n];
+            System.out.println("\nEnter element of array");
+            for(int i=0;i<n;i++){
+             a[i]=sc.nextInt();
+            }
+            int low=0;
+            int high=a.length;
+            int mid=(low+high)/2;
+            int b=a[mid];
+
+            System.out.println("\nMid element is:\n");
+
+            System.out.println(b);
+        }
+
+    }
+}
